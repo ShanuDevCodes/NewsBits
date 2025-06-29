17bbc2f9319986c3f2a63e7980940d4e0c81828b
ShanuDevCodes <shanu.dev.codes@gmail.com>
2025-06-29 06:31:42 +0530

    main ui

--- app/src/main/java/com/shanudevcodes/newsbits/MainActivity.kt
+++ app/src/main/java/com/shanudevcodes/newsbits/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.shanudevcodes.newsbits
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.navigation.compose.rememberNavController
 import com.shanudevcodes.newsbits.data.DataStoreManager
+import com.shanudevcodes.newsbits.ui.screens.EmptyScreen
 import com.shanudevcodes.newsbits.ui.screens.MainUi
 import com.shanudevcodes.newsbits.ui.theme.NewsBitsTheme
 import com.shanudevcodes.newsbits.ui.theme.ThemeOptions
@@ -17,6 +23,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val configuration = LocalConfiguration.current
+            val isPortrait = configuration.orientation == Configuration.ORIENTATION_PORTRAIT
             val dataStore = DataStoreManager(applicationContext)
             val themeOption by dataStore.themeFlow.collectAsState(initial = ThemeOptions.SYSTEM_DEFAULT)
             val navController = rememberNavController()
@@ -24,7 +32,22 @@ class MainActivity : ComponentActivity() {
                 themeOption = themeOption,
                 dynamicColor = false
             ) {
-                MainUi(navController)
+                if (!isPortrait) {
+                    Box() {
+                        Row {
+                            Box(modifier = Modifier.weight(0.35f)) {
+                                MainUi(navController)
+                            }
+                            Box(
+                                modifier = Modifier.weight(0.65f)
+                            ) {
+                                EmptyScreen()
+                            }
+                        }
+                    }
+                }else{
+                    MainUi(navController)
+                }
             }
         }
     }
