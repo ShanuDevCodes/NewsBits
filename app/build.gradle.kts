3543953c37bc0049f1f28f808245835c57b1315f
Kaneki.Uzumaki_ <sk5575723@gmail.com>
2025-07-03 23:18:40 +0530

    v2.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 30
         targetSdk = 35
         versionCode = 1
-        versionName = "2.2.2"
+        versionName = "2.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
