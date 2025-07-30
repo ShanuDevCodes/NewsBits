fac1809bf9619e394f999ede96b449dc0cd0b8bc
Kaneki.Uzumaki_ <shanu.dev.codes@gmail.com>
2025-07-31 03:30:39 +0530

    v5.2.2-Alpha
    -Bug Fixes
    -Implement Profile Screen
    -Implement Bit Digest Screen

--- app/src/main/java/com/shanudevcodes/newsbits/data/Destination.kt
+++ app/src/main/java/com/shanudevcodes/newsbits/data/Destination.kt
@@ -27,14 +27,6 @@ sealed class HomeDestination{
 }
 
 @Serializable
-sealed class BookmarkDestination{
-    @Serializable
-    object BOOKMARKSCREEN : BookmarkDestination()
-    @Serializable
-    data class BOOKMARKDETAILSCREEN(val newsId: String) : BookmarkDestination()
-}
-
-@Serializable
 sealed class SearchDestination{
     @Serializable
     object HOMESEARCHSCREEN : SearchDestination()
